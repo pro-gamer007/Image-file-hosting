d0cf937f4a8c7ea0c11cdd79a8ce453130c76fe3
pro_gamer007 <coolaryan@icloud.com>
2021-04-17 21:41:46 +0530

    user folders + support for videos (p1 hopefully finall)

--- index.js
+++ index.js
@@ -14,29 +14,56 @@ app.use(fileUpload({
 const reload = setInterval(function () { main() }, 2000);
 
 function main() {
-	const Files = fs.readdirSync('./public/Images/');
-	console.log('Reloaded')
+	let Filetype = 'img';
+	const Files = fs.readdirSync('./public/Images/pro');
 	for (const file of Files) {
 		const filename = file.split('.')[0];
 		app.get(`/${filename}`, function (req, res) {
-			res.render("Base", {
+			res.render(`pro${Filetype}`, {
 				file: file
 			})
 		});
 	}
+	let filetype = 'img';
+	const otherFiles = fs.readdirSync('./public/Images/nuha');
+	for (const file of otherFiles) {
+		const filename = file.split('.')[0];
+		const fileending = file.split('.')[1];
+		if (fileending == 'mp4' || fileending == 'mov') {
+			filetype = 'vid'
+		}
+		app.get(`/${filename}`, function (req, res) {
+			res.render(`nuha${filetype}`, {
+				file: file
+
+			})
+		});
+	}
+	console.log('Reloaded')
 }
 
 app.post('/upload', function (req, res) {
-	const rkey = process.env.key;
+	const prokey = process.env.prokey;
+	const nuhakey = req.query.nuhakey
 	const key = req.query.key;
-	if (key == rkey) {
+	let who;
+	let path;
+	if (key == prokey) {
+		who = 'pro';
+		path = 'public/Images/pro'
+	}
+	if (key == nuhakey) {
+		who = 'nuha'
+		path = 'public/Images/nuha' 
+	}
+	if (!key || !key == nuhakey || !key == prokey) {
 		res.status(401);
 		return res.send('Give a valid key bruh.');
 	}
 	const file = req.files.file;
 	const filename = file.name;
 	const url = filename.split('.')[0];
-	file.mv(`public/Images/${filename}`);
+	file.mv(`${path}/${filename}`);
 	return res.send(url);
 });
 
