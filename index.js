ea10574641f3f2dd92bfe0f32856415d18b181dc
pro_gamer007 <>
2021-04-17 05:06:19 +0000

    Files reloading and make it so images actually load

--- index.js
+++ index.js
@@ -3,15 +3,21 @@ const fs = require('fs');
 const app = express();
 
 app.set('view engine', 'ejs');
+app.use(express.static('public'));
 
-const Files = fs.readdirSync('./Images/');
-for (const file of Files) {
+const reload = setInterval(function(){ main() }, 2000);
+
+function main() {
+  const Files = fs.readdirSync('./public/Images/');
+  console.log('Reloaded')
+  for (const file of Files) {
   const filename = file.split('.')[0];
   app.get(`/${filename}`, function(req, res) {
     res.render("Base", {
       file: file
     })
 });
+}
 }
 
 app.listen(8080);
