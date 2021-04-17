1f8cdf96f15eadf3bf0e6427ee649d4c316fe2b8
pro_gamer007 <coolaryan@icloud.com>
2021-04-17 17:35:37 +0530

    possibly finall

--- index.js
+++ index.js
@@ -1,24 +1,44 @@
 const express = require('express');
 const fs = require('fs');
+const fileUpload = require('express-fileupload');
 const app = express();
+require('dotenv').config();
 
 app.set('view engine', 'ejs');
 app.use(express.static('public'));
+app.use(fileUpload({
+	safeFileNames: true,
+	preserveExtension: true
+}));
 
-const reload = setInterval(function(){ main() }, 2000);
+const reload = setInterval(function () { main() }, 2000);
 
 function main() {
-  const Files = fs.readdirSync('./public/Images/');
-  console.log('Reloaded')
-  for (const file of Files) {
-  const filename = file.split('.')[0];
-  app.get(`/${filename}`, function(req, res) {
-    res.render("Base", {
-      file: file
-    })
+	const Files = fs.readdirSync('./public/Images/');
+	console.log('Reloaded')
+	for (const file of Files) {
+		const filename = file.split('.')[0];
+		app.get(`/${filename}`, function (req, res) {
+			res.render("Base", {
+				file: file
+			})
+		});
+	}
+}
+
+app.post('/upload', function (req, res) {
+	const rkey = process.env.key;
+	const key = req.query.key;
+	if (key == rkey) {
+		res.status(401);
+		return res.send('Give a valid key bruh.');
+	}
+	const file = req.files.file;
+	const filename = file.name;
+	const url = filename.split('.')[0];
+	file.mv(`public/Images/${filename}`);
+	return res.send(url);
 });
-}
-}
 
 app.listen(8080);
 console.log('Server is now ready!');
